503273bcc7dfa8f13fcc82272a564f289e395071
arburhan <buthan190629@outlook.com>
2022-08-23 11:10:13 +0600

    create participate section

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from './Banner/Banner';
 import Statistics from './Statistics';
+import WhyParticipate from './Why Participate/WhyParticipate';
 
 const Home = () => {
     return (
         <>
             <Banner></Banner>
             <Statistics></Statistics>
+            <WhyParticipate></WhyParticipate>
         </>
     );
 };
---
+++ src/Pages/Home/Why Participate/WhyParticipate.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const WhyParticipate = () => {
+    return (
+        <section>
+            <h2 className="text-3xl">Why Participate in <span className='text-[#44924C]' >AI Challenges?</span> </h2>
+        </section>
+    );
+};
+
+export default WhyParticipate;
