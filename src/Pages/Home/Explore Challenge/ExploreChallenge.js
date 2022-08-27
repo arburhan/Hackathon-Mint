afd8d8b7ee83a0125f32ab74dacb78955914aea0
arburhan <buthan190629@outlook.com>
2022-08-27 20:08:15 +0600

    create responsive searchbar

--- src/Pages/Home/Explore Challenge/ExploreChallenge.js
+++ src/Pages/Home/Explore Challenge/ExploreChallenge.js
@@ -7,7 +7,7 @@ const ExploreChallenge = () => {
             <h3 className='text-center text-3xl text-white py-5' >Explore Challenge</h3>
             <div className='text-center' >
                 <div className='searchParent flex justify-center items-center' >
-                    <input className='rounded-md w-[50%] placeholder:font-[300] search-inputField' type="text" name="search" id="" placeholder='Search ' />
+                    <input className='rounded-md w-[75%] md:w-[50%] placeholder:font-[300] search-inputField' type="text" name="search" id="" placeholder='Search ' />
                     <span className='searchIcon' >
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                             <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
