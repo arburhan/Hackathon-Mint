5218e186e6cb5350f442be5b2a1c0eb26383ad88
arburhan <buthan190629@outlook.com>
2023-01-25 12:58:15 +0600

    fix responsive issues

--- src/Pages/Home/Explore Challenge/ChallengeCard.js
+++ src/Pages/Home/Explore Challenge/ChallengeCard.js
@@ -7,12 +7,15 @@ const ChallengeCard = ({ data }) => {
             <figure className="">
                 <img src={data?.image} alt="Shoes" className="" />
             </figure>
-            <div className="card-body items-center text-center py-7 bg-white text-black ">
-                <p></p>
-                <h2 className=" text-base font-bold">{data?.name}</h2>
-                <CountDown />
+            <div className="card-body items-center text-center gap-0 py-7 bg-white text-black ">
+                <p>Coming</p>
+                <h2 className=" text-base font-bold pt-4 pb-5">{data?.name}</h2>
+                <div className='pb-8' >
+                    <p className='pb-2 text-[#444444]'>Starts in</p>
+                    <CountDown />
+                </div>
                 <div className="">
-                    <button className="bg-[#44924C] text-white py-3 px-6 rounded-md">
+                    <button className="bg-[#44924C] text-white py-3 px-6 rounded-xl">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 inline pr-1">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                         </svg>Perticipate Now</button>
--- src/Pages/Home/Explore Challenge/ExploreChallenge.js
+++ src/Pages/Home/Explore Challenge/ExploreChallenge.js
@@ -47,22 +47,22 @@ const ExploreChallenge = () => {
             {/* search & filter option */}
             <div className='bg-[#002A3B] relative pt-5 pb-24'>
                 <h3 className='text-3xl text-white py-10 text-center' >Explore Challenge</h3>
-                <div className='pl-[150px] flex gap-4' >
+                <div className='lg:pl-[150px] md:pl-[70px] flex flex-col md:flex-row items-center  md:items-start gap-4' >
                     <div className='relative' >
                         <span className='absolute top-2 left-3' >
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
                             </svg>
                         </span>
-                        <input className='rounded-md w-[830px] placeholder:font-[300] search-inputField' type="text" name="search" id="" placeholder='Search ' />
+                        <input className='rounded-md  md:w-[380px] lg:w-[580px] xl:w-[830px] placeholder:font-[300] search-inputField' type="text" name="search" id="" placeholder='Search ' />
                     </div>
-                    <div className=''>
+                    <div>
                         {dropdown === false ?
                             <div
                                 onClick={() => {
                                     setDropdown(true)
                                 }}
-                                className='flex duration-100   absolute bg-white p-2 rounded-md text-black cursor-pointer' >
+                                className='flex duration-100   absolute bg-white p-2 rounded-md text-black cursor-pointer smFilterBtn' >
                                 <p className='px-2' >Filter</p>
                                 <figure className='pl-2' >
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
@@ -72,7 +72,7 @@ const ExploreChallenge = () => {
                             </div>
                             :
                             <div
-                                className='duration-100  w-[290px] bg-white pl-4 pt-6 pb-3 rounded-md text-black absolute z-10 shadow-2xl' >
+                                className='duration-100 w-[200px] md:w-[290px] lg:w-[260px] xl:w-[290px] bg-white pl-4 pt-6 pb-3 rounded-md text-black absolute z-10  shadow-2xl smFilterBtn' >
                                 <div
                                     onClick={() => {
                                         setDropdown(false)
@@ -85,7 +85,7 @@ const ExploreChallenge = () => {
                                         </svg>
                                     </figure>
                                 </div>
-                                <hr className='px-2 text-[#ECECEC] w-[265.01px]' />
+                                <hr className='px-2 text-[#ECECEC] w-[170px] xl:w-[265.01px]' />
                                 <div className='py-4' >
                                     <fieldset>
                                         <legend className='text-[#666666]' >Status</legend>
@@ -94,7 +94,7 @@ const ExploreChallenge = () => {
                                         }
                                     </fieldset>
                                 </div>
-                                <hr className='px-2 text-[#ECECEC] w-[265.01px]' />
+                                <hr className='px-2 text-[#ECECEC] w-[170px] xl:w-[265.01px]' />
                                 <div className='py-4'>
                                     <fieldset>
                                         <legend className='text-[#666666]' >Level</legend>
@@ -109,7 +109,7 @@ const ExploreChallenge = () => {
                 </div>
             </div>
             {/* challenges */}
-            <div className='grid grid-cols-3 justify-items-center gap-y-14 pt-20 pb-32 bg-[#003145]' >
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 justify-items-center gap-y-14 pt-20 pb-32 bg-[#003145]' >
                 {
                     challengeData.map(data => <ChallengeCard key={data?._id} data={data} ></ChallengeCard>)
                 }
