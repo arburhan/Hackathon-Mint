1738215ce4d5bb425de71ea03b0b76f297554c12
arburhan <burhan128454@outlook.com>
2023-03-06 12:28:26 +0600

    single challenge

--- src/Pages/Home/Explore Challenge/ChallengeDetails.js
+++ src/Pages/Home/Explore Challenge/ChallengeDetails.js
@@ -1,9 +1,10 @@
+import moment from 'moment/moment';
 import React, { useEffect, useState } from 'react';
 import { useLocation, useParams } from 'react-router-dom';
 import { challengeData } from './challengeData';
 
 const ChallengeDetails = () => {
-    const [cardId, setCardId] = useState([]);
+    const [challengedata, setChallengeData] = useState([]);
     const { id } = useParams();
     console.log(id);
     // bring to top
@@ -13,16 +14,26 @@ const ChallengeDetails = () => {
     }, [pathname]);
     useEffect(() => {
         const filterData = challengeData.filter((obj) => obj?._id === parseInt(id));
-        setCardId(filterData);
+        setChallengeData(filterData);
     }, [id]);
-    console.log(cardId);
+    console.log(challengedata);
 
 
     return (
-        <div>
+        <section>
+
+            <div>
+                <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg>
+                    Starts on {moment().format("MMM Do YY")}
+
+                </div>
+            </div>
 
 
-        </div>
+        </section>
     );
 };
 
